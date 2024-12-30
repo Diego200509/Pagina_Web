2bd2e4d79585dccdf42f4e305ea7524377fe36f6
Diego <diegojijon2005@gmail.com>
2024-12-30 15:49:20 -0500

    Se mejoro el diseño del navbar para que sea administrable.

--- Eventos_Noticias/eventos_noticias.php
+++ Eventos_Noticias/eventos_noticias.php
@@ -10,20 +10,32 @@
 </head>
 
 <body>
-    <header>
-        <div class="logo">
-            <img src="../Home/Img/logo.png" alt="UTA Logo">
-            <h1>Proceso de Elecciones UTA 2024</h1>
-        </div>
-        <nav>
-            <a href="../Home/inicio.php"><i class="fas fa-home"></i> Inicio</a>
-            <a href="../Candidatos/candidatos.php"><i class="fas fa-user"></i> Candidatos</a>
-            <a href="../Propuestas/Propuestas.php"><i class="fas fa-bullhorn"></i> Propuestas</a>
-            <a href='../Eventos_Noticias/eventos_noticias.php'><i class="fas fa-calendar-alt"></i> Eventos y
-                Noticias</a>
-            <a href="../Sugerencias/index.php"><i class="fas fa-comment-dots"></i> Sugerencias</a>
+    <navbar>
+        <!-- Navbar -->
+        <nav class="navbar">
+            <div class="navbar-logo">
+                <div class="text-center">
+                </div>
+                <!-- Logo existente -->
+                <img src="../Login/Img/logoMariCruz.png" width="200px" style="margin-right: 20px;">
+
+            </div>
+
+
+
+            </div>
+            <ul class="navbar-menu">
+                <li><a href="../Home/inicio.php"><i class="fa-solid fa-house"></i> <span>Inicio</span></a></li>
+                <li><a href="../Candidatos/candidatos.php"><i class="fa-solid fa-users"></i> <span>Candidatos</span></a></li>
+                <li><a href="../Eventos_Noticias/eventos_noticias.php"><i class="fa-solid fa-calendar-alt"></i> <span>Eventos y Noticias</span></a></li>
+                <li><a href="../Propuestas/Propuestas.php"><i class="fa-solid fa-lightbulb"></i> <span>Propuestas</span></a></li>
+                <li><a href="../Sugerencias/index.php"><i class="fa-solid fa-comment-dots"></i> <span>Sugerencias</span></a></li>
+                <li><a href="../Sugerencias/resultados.php"><i class="fas fa-vote-yea"></i> Votos</a></li>
+            </ul>
         </nav>
-    </header>
+
+
+    </navbar>
 
     <div id="events" class="content-section">
         <h2>Eventos</h2>
