dcbd900f2ca1cc4fa90e5559db880b9cae6396ec
leonel <lozadaleonel15@gmail.com>
2025-01-03 14:55:16 -0500

    Mejoras css en candidatos vista usuario

--- Candidatos/candidatos.php
+++ Candidatos/candidatos.php
@@ -1,6 +1,16 @@
 <?php
-// Manejo de la lógica del backend aquí, si es necesario
-// Esto puede incluir validaciones o configuraciones específicas
+include('../config/config.php');
+
+
+$navbarConfigPath = "../Login/navbar_config.json"; 
+
+if (file_exists($navbarConfigPath)) {
+    $navbarConfig = json_decode(file_get_contents($navbarConfigPath), true);
+    $navbarBgColor = $navbarConfig['navbarBgColor'] ?? '#00bfff'; 
+} else {
+    $navbarBgColor = '#00bfff'; 
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -11,23 +21,42 @@
     <link rel="stylesheet" href="candidatos_usuarios.css">
     <link rel="stylesheet" href="candidatos_usuarios.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<style>
+        :root {
+            --navbar-bg-color: <?php echo $navbarBgColor; ?>;
+        }
+    </style>
 </head>
 
 <body>
-    <!-- Header -->
-    <header>
-        <div class="logo">
-            <img src="../Home/Img/logo.png" alt="UTA Logo">
-            <h1>Proceso de Elecciones UTA 2024</h1>
-        </div>
-        <nav>
-            <a href="../Home/inicio.php"><i class="fas fa-home"></i> Inicio</a>
-            <a href="../Candidatos/candidatos.php"><i class="fas fa-user"></i> Candidatos</a>
-            <a href="../Propuestas/Propuestas.php"><i class="fas fa-bullhorn"></i> Propuestas</a>
-            <a href="../Eventos_Noticias/eventos_noticias.php"><i class="fas fa-calendar-alt"></i> Eventos y Noticias</a>
-            <a href="../Sugerencias/index.php"><i class="fas fa-comment-dots"></i> Sugerencias</a>
+    <navbar>
+        <!-- Navbar -->
+        <nav class="navbar">
+            <div class="navbar-logo">
+                <div class="text-center">
+                </div>
+                <img src="../Login/Img/logoMariCruz.png" width="200px" style="margin-right: 20px;">
+
+            </div>
+
+
+
+            </div>
+            <ul class="navbar-menu">
+                <li><a href="../Home/inicio.php"><i class="fa-solid fa-house"></i> <span>Inicio</span></a></li>
+                <li><a href="../Candidatos/candidatos.php"><i class="fa-solid fa-users"></i> <span>Candidatos</span></a></li>
+                <li><a href="../Eventos_Noticias/eventos_noticias.php"><i class="fa-solid fa-calendar-alt"></i> <span>Eventos y Noticias</span></a></li>
+                <li><a href="../Propuestas/Propuestas.php"><i class="fa-solid fa-lightbulb"></i> <span>Propuestas</span></a></li>
+                <li><a href="../Sugerencias/candidato1.php"><i class="fa-solid fa-comment-dots"></i> <span>Sugerencias</span></a></li>
+                <li><a href="../Sugerencias/votos.php"><i class="fas fa-vote-yea"></i> Votos</a></li>
+            </ul>
         </nav>
-    </header>
+
+
+    </navbar>
     <main>
     <h2 class="title">Información del Candidato</h2>
 
